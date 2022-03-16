0c4b8ec2b734b0bc0f9184ea44e2f3ea7f6cf78b
Roman <the.red.lemon@yandex.ru>
2022-03-16 11:33:15 +0300

    proccesTab

--- WindowAndDoors/src/js/script.js
+++ WindowAndDoors/src/js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
         .addClass('product__tab_active').siblings().removeClass('product__tab_active')
         .closest('div.container').find('div.product__content').removeClass('product__content_active').eq($(this).index()).addClass('product__content_active');
     });
+
+    //Rehau
+    $('ul.rehau__tabs').on('click', 'li:not(.rehau__tab_active)', function() {
+        $(this)
+        .addClass('rehau__tab_active').siblings().removeClass('rehau__tab_active')
+        .closest('div.rehau').find('div.rehau__descr').removeClass('rehau__descr_active').eq($(this).index()).addClass('rehau__descr_active');
+    });
+
 });
